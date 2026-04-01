0e54d3a7bb18f04e100342af1808a197623c76cd
transatlanticvoyage <sandbagexpert@gmail.com>
2026-04-01 14:59:19 -0400

    dsf

--- headers/header2/assets/js/scripts.js
+++ headers/header2/assets/js/scripts.js
@@ -7,7 +7,7 @@
 jQuery(document).ready(function($) {
     
     // Sticky header functionality - Header2 specific implementation
-    var header = $('.hs2-header[data-sticky="true"]');
+    var header = $('.zx_hd2_header[data-sticky="true"]');
     if (header.length) {
         var headerOffset;
         var isSticky = false;
@@ -22,37 +22,37 @@ jQuery(document).ready(function($) {
             var scrollTop = $(window).scrollTop();
             
             if (scrollTop > headerOffset && !isSticky) {
-                header.addClass('hs2-sticky-active');
+                header.addClass('zx_hd2_sticky_active');
                 isSticky = true;
             } else if (scrollTop <= headerOffset && isSticky) {
-                header.removeClass('hs2-sticky-active');
+                header.removeClass('zx_hd2_sticky_active');
                 isSticky = false;
             }
         });
     }
     
     // Header2-specific animations and effects
-    var invisibleHeader = $('.hs2-header.hs2-invisible');
+    var invisibleHeader = $('.zx_hd2_header.zx_hd2_invisible');
     if (invisibleHeader.length) {
         setTimeout(function() {
-            invisibleHeader.removeClass('hs2-invisible');
+            invisibleHeader.removeClass('zx_hd2_invisible');
         }, 100);
     }
     
     // Header2-specific hover effects for desktop
     if ($(window).width() > 1024) {
-        $('.hs2-menu-item').hover(
+        $('.zx_hd2_menu_item').hover(
             function() {
-                $(this).addClass('hs2-hover');
+                $(this).addClass('zx_hd2_hover');
             },
             function() {
-                $(this).removeClass('hs2-hover');
+                $(this).removeClass('zx_hd2_hover');
             }
         );
     }
     
     // Header2-specific phone button tracking (if analytics enabled)
-    $('.hs2-phone-button').on('click', function() {
+    $('.zx_hd2_phone_button').on('click', function() {
         if (typeof gtag !== 'undefined') {
             gtag('event', 'click', {
                 'event_category': 'Header2',
@@ -63,9 +63,9 @@ jQuery(document).ready(function($) {
     });
     
     // Header2-specific accessibility enhancements
-    $('.hs2-menu-link').on('focus', function() {
-        $(this).closest('.hs2-menu-item').addClass('hs2-focused');
+    $('.zx_hd2_menu_link').on('focus', function() {
+        $(this).closest('.zx_hd2_menu_item').addClass('zx_hd2_focused');
     }).on('blur', function() {
-        $(this).closest('.hs2-menu-item').removeClass('hs2-focused');
+        $(this).closest('.zx_hd2_menu_item').removeClass('zx_hd2_focused');
     });
 });
